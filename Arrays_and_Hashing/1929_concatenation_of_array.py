629fafbf866a1cf6bdde382e54bb2b11d2c67a8e
Nilesh Saudagar Kamble <106882346+TheIllustrator1972@users.noreply.github.com>
2025-06-07 15:20:41 +0530

    Update 1929_concatenation_of_array.py

--- Arrays_and_Hashing/1929_concatenation_of_array.py
+++ Arrays_and_Hashing/1929_concatenation_of_array.py
@@ -16,3 +16,15 @@ class Solution:
             for j in range(len(nums)):
                 ans.append(nums[j])
         return ans
+
+# Another way to iterate directly
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(2):
+            for j in nums:
+                ans.append(j)
+        return ans
+
+# For index and value
+for idx, item in enumerate(my_list):
