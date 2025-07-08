3bc0d9a4679934ce820aad68dc9ec0a5204ed997
Nilesh Saudagar Kamble <106882346+TheIllustrator1972@users.noreply.github.com>
2025-07-08 22:25:32 +0530

    Update 232_implement_queue_using_stacks.py

--- Stack/232_implement_queue_using_stacks.py
+++ Stack/232_implement_queue_using_stacks.py
@@ -20,3 +20,29 @@ class MyQueue:
 
     def empty(self) -> bool:
         return len(self.s) == 0
+
+
+#  O(1) Solution
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        self.s1.append(x)
+
+    def pop(self) -> int:
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2.pop()
+        
+    def peek(self) -> int:
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]
+
+    def empty(self) -> bool:
+        return not self.s1 and not self.s2
