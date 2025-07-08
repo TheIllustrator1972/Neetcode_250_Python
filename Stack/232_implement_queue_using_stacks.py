afa6b31cd98eb4e3cd68272aaf76b6c1a0483433
Nilesh Saudagar Kamble <106882346+TheIllustrator1972@users.noreply.github.com>
2025-07-08 22:09:45 +0530

    Create 232_implement_queue_using_stacks.py

---
+++ Stack/232_implement_queue_using_stacks.py
@@ -0,0 +1,22 @@
+class MyQueue:
+    def __init__(self):
+        self.s = []
+        self.extra = []
+
+    def push(self, x: int) -> None:
+        self.s.append(x)
+
+    def pop(self) -> int:
+        for i in range(len(self.s)):
+            self.extra.append(self.s.pop())
+        ans = self.extra[-1]
+        self.extra.pop()
+        while self.extra:
+            self.s.append(self.extra.pop())
+        return ans
+        
+    def peek(self) -> int:
+        return self.s[0]
+
+    def empty(self) -> bool:
+        return len(self.s) == 0
