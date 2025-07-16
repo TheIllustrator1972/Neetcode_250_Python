5b22b3a8224ffa8af379dbde0a4ba9d1edde8172
Nilesh Saudagar Kamble <106882346+TheIllustrator1972@users.noreply.github.com>
2025-07-16 21:52:26 +0530

    Update 33_search_in_rotated_sorted_array.py

--- Binary_Search/33_search_in_rotated_sorted_array.py
+++ Binary_Search/33_search_in_rotated_sorted_array.py
@@ -1,3 +1,4 @@
+# O(n) Approach
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         pivot = 0
@@ -26,4 +27,28 @@ class Solution:
         presentInRight = binarySearchInRange(pivot + 1, len(nums) - 1)
         if presentInRight != -1: 
             return presentInRight
+        return -1
+
+# Modified Binary Search
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l , r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            
+            # Left Portion
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            # Right Sorted Portion
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
         return -1
